1b09e19cc5b781460e33138c875ab86cfec50c97
Bill Martensson <bill@magictechnology.se>
2018-02-08 09:23:34 +0100

    Initial Commit

---
+++ apia2v1tors/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  apia2v1tors
+//
+//  Created by Bill Martensson on 2018-02-08.
+//  Copyright © 2018 Magic Technology. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
